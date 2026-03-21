0dda6c6014c40475e2fc0c503abb4448e74b9972
gkhcsc <2640922004@qq.com>
2026-03-21 17:19:23 +0800

    init

---
+++ index.mjs
@@ -0,0 +1,18 @@
+import express from 'express';
+import {exec} from "child_process";
+const app = express();
+app.use(express.json());
+const CONFIG = {
+    port:8000
+}
+
+app.post("/", (req, res) => {
+    console.log(req.body);
+    res.send("ok");
+})
+
+
+
+app.listen(CONFIG.port, () => {
+    console.log(`Server running on port ${CONFIG.port}`);
+});
